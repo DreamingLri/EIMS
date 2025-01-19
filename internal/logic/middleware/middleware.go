1c62c1dafe3e8bed5659e82b682a98c9ea50144d
DreamingLri <3507415455@qq.com>
2025-01-19 17:31:18 +0800

    ✨ 添加CROS中间件

---
+++ internal/logic/middleware/middleware.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"demo/internal/service"
+	"github.com/gogf/gf/v2/net/ghttp"
+)
+
+type sMiddleware struct{}
+
+func init() {
+	service.RegisterMiddleware(New())
+}
+
+func New() *sMiddleware {
+	return &sMiddleware{}
+}
+
+func (s *sMiddleware) CORS(r *ghttp.Request) {
+	r.Response.CORSDefault()
+	r.Middleware.Next()
+}
+
+func (s *sMiddleware) ResponseHandler(r *ghttp.Request) {
+	r.Middleware.Next()
+}
